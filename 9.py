79af50f24e21e5b54a8a47c9d0b9535cdb0aeaf2
RandomUsername96 <koorweetzaah2@gmail.com>
2015-12-09 21:20:52 +0100

    minor change on day 9

--- 9.py
+++ 9.py
@@ -25,7 +25,7 @@ lines = in_file.readlines()
 it = 0
 for i in range(0, 8):
 	for j in range(i+1, 8):
-		MASK = (1<<i) + (1<<j)
+		MASK = (1<<i) | (1<<j)
 		DP[MASK][i][0] = DP[MASK][j][0] = int(lines[it].split()[4])
 		DP[MASK][i][1] = DP[MASK][j][1] = int(lines[it].split()[4])
 		it += 1
@@ -44,16 +44,16 @@ for i in range(3, 9):
 					for nBit in range(0, 8):
 						if nMASK & (1 << nBit):
 						# make a nBit -> bit transition and update DP[MASK][bit]
-							newDist = DP[nMASK][nBit][0] + DP[(1<<bit) + (1<<nBit)][bit][0]
+							newDist = DP[nMASK][nBit][0] + DP[(1<<bit) | (1<<nBit)][bit][0]
 							DP[MASK][bit][0] = min(newDist, DP[MASK][bit][0])
-							newDist = DP[nMASK][nBit][1] + DP[(1<<bit) + (1<<nBit)][bit][1]
+							newDist = DP[nMASK][nBit][1] + DP[(1<<bit) | (1<<nBit)][bit][1]
 							DP[MASK][bit][1] = max(newDist, DP[MASK][bit][1])
 
 
 # find the answer, best among DP[MASKS-1][]
 ret = [INF, 0]
 for lst in range(0, 8):
-	ret[0] = min(DP[MASKS- 1][lst][0], ret[0])
+	ret[0] = min(DP[MASKS - 1][lst][0], ret[0])
 	ret[1] = max(DP[MASKS - 1][lst][1], ret[1])
 
 # print the solution
